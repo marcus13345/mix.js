88a35bb4a7e3599df32c8b9ced91e562cd7499ac
Marcus <marcus@valnet.xyz>
2020-10-18 10:32:41 -0400

    dont do packages

--- webpack/custom-file-loader/index.js
+++ webpack/custom-file-loader/index.js
@@ -4,7 +4,13 @@ module.exports = function fallbackLoader (source) {
 
 
 	if (this.loaders.length !== 1) return source;
-	if (this.loaderIndex !== 0) return source
+	if (this.loaderIndex !== 0) return source;
+
+	// JSON breaks for some reason. it only has one loader, this
+	// but it doesnt need to be transformed. the default non-existant
+	// loader works fine...
+	// js files would likely have the same issue if we weren't babelling them.
+	if (this.resourcePath.endsWith('.json')) return source;
 
 	// console.log()
 	// console.log(this.resourcePath)
